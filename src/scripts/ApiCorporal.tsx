59373f0c4131dd087dd76a87e34c8b52071b6749
Sebastian Cerviño <cervisebas@gmail.com>
2022-04-17 16:11:55 -0300

    17/04/2022 16:11hs

--- src/scripts/ApiCorporal.tsx
+++ src/scripts/ApiCorporal.tsx
@@ -4,8 +4,8 @@ import CommentSystem from "./ApiCorporal/comments";
 import PermissionSystem from "./ApiCorporal/permissions";
 import TrainingSystem from "./ApiCorporal/training";
 
-const HostServer: string = 'http://192.168.1.36/CorporalKinesisApi';
-//const host: string = 'https://api.corporalkinesis.com.ar';
+//const HostServer: string = 'http://192.168.1.36/CorporalKinesisApi';
+const HostServer: string = 'https://api.corporalkinesis.com.ar';
 const keyAccess: string = encode('pFQVXt&yC%aa8e-^&cY4FRtXm&s87$6%3+6D+REGK4bQNLeY');
 
 const Account = new AccountSystem(HostServer, keyAccess);

d88334b19ddc61913fc04b65f3db52b53fd32954
Sebástian Cerviño García <cervisebas@gmail.com>
2023-01-18 17:31:40 -0300

    18/01/2023 17:31hs

--- src/screens/pagesProfesional/pages/editClient.tsx
+++ src/screens/pagesProfesional/pages/editClient.tsx
@@ -2,7 +2,8 @@ import React, { forwardRef, useImperativeHandle, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import CustomModal from "../../components/CustomModal";
 import CombinedTheme from "../../../Theme";
-import { Appbar } from "react-native-paper";
+import { Appbar, TextInput } from "react-native-paper";
+import { ScrollView } from "react-native-gesture-handler";
 
 type IProps = {};
 export type EditClientProfessionalRef = {
@@ -11,6 +12,13 @@ export type EditClientProfessionalRef = {
 
 export default React.memo(forwardRef(function EditClientProfessional(_props: IProps, ref: React.Ref<EditClientProfessionalRef>) {
     const [visible, setVisible] = useState(false);
+    const [name, setName] = useState('');
+    const [dni, setDNI] = useState('');
+    const [phone, setPhone] = useState('');
+    const [birthday, setBirthday] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
 
     function close() {
         setVisible(false);
@@ -27,9 +35,12 @@ export default React.memo(forwardRef(function EditClientProfessional(_props: IPr
                 <Appbar.BackAction onPress={close} />
                 <Appbar.Content title={'Editar usuario'} />
             </Appbar.Header>
-            <View style={styles.content}>
-                
-            </View>
+            <ScrollView style={{ flex: 1 }} contentContainerStyle={styles.scrollView}>
+                <TextInput label={'Nombre y Apellido'} style={styles.textInput} mode={'outlined'} value={name} onChangeText={setName} autoCapitalize={'words'} keyboardType={'default'} textContentType={'name'} blurOnSubmit={false} />
+                <TextInput label={'D.N.I'} style={styles.textInput} mode={'outlined'} value={dni} onChangeText={setDNI} keyboardType={'numeric'} blurOnSubmit={false} />
+                <TextInput label={'Teléfono'} style={styles.textInput} mode={'outlined'} value={phone} onChangeText={setPhone} keyboardType={'phone-pad'} textContentType={'telephoneNumber'} blurOnSubmit={false} />
+                <TextInput label={'E-Mail'} style={styles.textInput} mode={'outlined'} value={email} onChangeText={setEmail} autoCapitalize={'none'} keyboardType={'email-address'} textContentType={'emailAddress'} blurOnSubmit={false} />
+            </ScrollView>
         </View>
     </CustomModal>);
 }));
@@ -41,5 +52,14 @@ const styles = StyleSheet.create({
     content: {
         flex: 1,
         backgroundColor: CombinedTheme.colors.background
+    },
+    scrollView: {
+        paddingTop: 12,
+        paddingLeft: 8,
+        paddingRight: 8,
+        paddingBottom: 24
+    },
+    textInput: {
+        marginBottom: 8
     }
 });

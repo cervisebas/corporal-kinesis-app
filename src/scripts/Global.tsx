e3e46e47623e89ac606a0b221e032b843405f907
Sebástian Cerviño García <cervisebas@gmail.com>
2023-04-08 17:57:49 -0300

    08/04/2023 17:57hs

--- src/scripts/Global.tsx
+++ src/scripts/Global.tsx
@@ -1,31 +1,3 @@
-import React, { Component } from "react";
-import LoadingController from "../components/loading/loading-controller";
-import CombinedTheme from "../Theme";
-
-type IProps = {
-    loadingView: boolean;
-    loadingText: string;
-};
-type IState = {};
-
-export class Global extends Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-    }
-    render(): React.ReactNode {
-        return(<>
-            <LoadingController
-                show={this.props.loadingView}
-                loadingText={this.props.loadingText}
-                backgroundColor={'#323335'}
-                indicatorColor={CombinedTheme.colors.accent}
-                colorText={CombinedTheme.colors.text}
-                borderRadius={8}
-            />
-        </>);
-    }
-};
-
 var LoadNow: boolean = false;
 var setLoadNow: (data: boolean)=>any = (data)=>LoadNow = data;
 export { LoadNow, setLoadNow };
